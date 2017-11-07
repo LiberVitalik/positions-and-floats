ab88c32c6f002a84e7b8aea44e4a4eb08f9fb3ef
Vitali Liber <libervitalik@gmail.com>
2017-11-07 15:50:26 +0300

    Fix after review

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
-    babel = require('gulp-babel'),
     autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
@@ -9,16 +8,6 @@ gulp.task('sass', function () {
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('babel', function () {
-    return gulp.src('app/babel/**/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-            /*plugins: ['transform-runtime']*/
-        }))
-        .pipe(gulp.dest('app/js'))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -31,13 +20,9 @@ gulp.task('browser-sync', function () {
     })
 });
 
-gulp.task('watch', ['browser-sync', 'babel', 'sass'], function () {
+gulp.task('watch', ['browser-sync', 'sass'], function () {
     gulp.watch('app/sass/**/*.+(scss|sass)', ['sass']);
-    gulp.watch('app/babel/**/*.js', ['babel']);
-    gulp.watch('app/babel/**/*.js', ['babel']);
     gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-    gulp.watch('app/babel/**/*.js', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
